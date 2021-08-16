ce413a99edaee69102c9104499753e2e3e3d4f28
IvanKwabena <ikasante003@st.ug.edu.gh>
2021-08-16 16:34:40 +0000

    Added extra files

--- Settings/config.py
+++ Settings/config.py
@@ -18,8 +18,8 @@ envs = ['PROD', 'DEV']
 
 
 
-#server
-host = '0.0.0.0'
-port = int(os.environ.get("PORT", 5000))
-webhook = 'https://bot-date-reminder.herokuapp.com/'
+# #server
+# host = '0.0.0.0'
+# port = int(os.environ.get("PORT", 5000))
+# webhook = 'https://bot-date-reminder.herokuapp.com/'
 
